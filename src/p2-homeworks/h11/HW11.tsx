31e6b7bdccf105a6a523b8b3eaffb3af05f85082
Semen Kozhin <sa.kozh@yandex.ru>
2021-10-30 13:56:55 +0200

    28.10.2021

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -5,7 +5,19 @@ import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
-
+    // const onChangeDoubleRange = (values: Array<number>) => {
+    //     if (values[2] === 0){
+    //         setValue1(values[0])
+    //     } else {
+    //         setValue2(values[1])
+    //     }
+    // }
+    const onLeftChangeRange = (value: number) => {
+        setValue1(value)
+    }
+    const onRightChangeRange = (value: number) => {
+        setValue2(value)
+    }
     return (
         <div>
             <hr/>
@@ -15,16 +27,21 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
-                <span>{value1}</span>
+                <div>{value1}</div>
                 <SuperDoubleRange
+                    value={[value1, value2]}
+                    onLeftChangeRange={onLeftChangeRange}
+                    onRightChangeRange={onRightChangeRange}
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <span>{value2}</span>
+                <div>{value2}</div>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import s from "./SuperDoubleRange.module.css"
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
+    onLeftChangeRange?: (value: number) => void
+    onRightChangeRange?: (value: number) => void
     value?: [number, number]
     // min, max, step, disable, ...
 }
@@ -9,14 +12,26 @@ type SuperDoubleRangePropsType = {
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
+        onLeftChangeRange,
+        onRightChangeRange
         // min, max, step, disable, ...
     }
 ) => {
+    const onLeftChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onLeftChangeRange && onLeftChangeRange(+e.currentTarget.value)
+    }
+    const onRightChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onRightChangeRange && onRightChangeRange(+e.currentTarget.value)
+    }
     // сделать самому, можно подключать библиотеки
 
     return (
         <>
-            DoubleRange
+            <div>
+            <input onChange={onLeftChangeCallback} type="range" min="0" max="100" value={value ? value[0] : 0} className={s.slider} id="lower"/>
+                <input onChange={onRightChangeCallback} type="range" min="0" max="100" value={value ? value[1] : 100} className={s.slider} id="higher"/>
+            </div>
+
         </>
     )
 }
