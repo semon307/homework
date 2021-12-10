ac2e091220f191546098264d48910ec84c3bdc03
Semen Kozhin <sa.kozh@yandex.ru>
2021-12-10 12:04:09 +0100

    10.12

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HW7 from "../../h7/HW7";
 import {HW13} from "../../h13/HW13";
+import HW12 from "../../h12/HW12";
 
 export const JuniorPlus = () => {
     return (
         <div>
+            <HW12/>
             <HW13/>
         </div>
     )
